71bd3073726988b6879870bcd88dc25ceb840695
Walkowiak <wafelek130@gmail.com>
2025-09-03 12:20:52 +0200

    03.09.2025

--- bazyDanych/01_raporty/kwerendy.sql
+++ bazyDanych/01_raporty/kwerendy.sql
@@ -1,7 +1,14 @@
 -- - Zapytanie 1: wybierające jedynie pola imie, telefon, szczepienie, opis z tabeli Zwierzeta
-
+SELECT imie, telefon, szczepienie, opis
+FROM Zwierzeta;
 -- - Zapytanie 2: wybierające jedynie pola id, imie, wlasciciel z tabeli Zwierzeta dla tych rekordów, dla których rodzaj to pies
-
+SELECT id, imie, wlasciciel
+FROM Zwierzeta
+WHERE rodzaj = 1;
 -- - Zapytanie 3: korzystające z relacji i wybierające jedynie pola imie z tabeli Zwierzeta oraz odpowiadające im pola nazwa z tabeli Uslugi
-
+SELECT imie, nazwa
+FROM Zwierzeta
+    JOIN uslugi ON uslugi.id = zwierzeta.usluga_id;
 -- - Zapytanie 4: zwracające średnią cenę wszystkich usług zapisanych w tabeli Uslugi
+SELECT AVG(cena)
+FROM Uslugi;
---
+++ tworzenieIZarzadzanieBazamiDanych/01_raporty/weterynarz.php
@@ -0,0 +1,84 @@
+<?php
+$link = new mysqli('localhost','root','','weterynarzWW');
+$sql = "SELECT id, imie, wlasciciel
+FROM Zwierzeta
+WHERE rodzaj = 1";
+$result = $link -> query($sql);
+$dogs = $result -> fetch_all(1);
+
+$sql = "SELECT id, imie, wlasciciel
+FROM Zwierzeta
+WHERE rodzaj = 2";
+$result = $link -> query($sql);
+$cats = $result -> fetch_all(1);
+
+$sql = "SELECT imie, telefon, szczepienie, opis
+FROM Zwierzeta";
+$result = $link -> query($sql);
+$animals = $result -> fetch_all(1);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="weterynarz.css">
+</head>
+<body>
+    <header></header>
+
+    <main>
+        <section class="left">
+            <h2>Psy</h2>
+            <!-- skrypt1 -->
+            <?php 
+                foreach($dogs as $dog){
+                    echo "{$dog['id']} {$dog['imie']} {$dog['wlasciciel']} <br>";
+                }
+            ?>
+            <!-- id imie wlasciciel <br> -->
+            <h2>Koty</h2>
+            <?php
+                foreach($cats as $cat){
+                    echo "{$cat['id']} {$cat['imie']} {$cat['wlasciciel']} <br>";
+                }
+            ?>
+            <!-- skrypt2 -->
+        </section>
+        <section class="middle">
+            <h2>SZCZEGÓŁOWA INFORMACJA O ZWIERZĘTACH</h2>
+            <!-- Pacjent: <imie> <br>
+            Telefon właściciela: <telefon>, ostatnie szczepienie: <szczepienie>, informacje: <opis> <hr> -->
+            <!-- skrypt3 -->
+            <?php
+            foreach($animals as $animal){
+                echo "Pacjent: {$animal['imie']} <br>Telefon właściciela: {$animal['telefon']}, {$animal['szczepienie']}, informacje: {$animal['opis']} <hr>";
+            }
+            ?>
+            
+        </section>
+        <section class="right">
+        <h2>WETERYNARZ</h2>
+        <a href="rys.png"><img src="rys-mini.png" alt="ladny piesek" srcset=""></a>
+        <p>Krzysztof Nowakowski, lekarz weterynarii</p>
+        <h2>GODZINY PRZYJĘĆ</h2>
+        <table>
+            <tr>
+                <td>Poniedziałek</td>
+                <td>15:00 - 19:00</td>
+            </tr>
+            <tr>
+                <td>Wtorek</td>
+                <td>15:00 - 19:00</td>
+            </tr>
+        </table>
+        </section>
+    </main>
+</body>
+</html>
+
+<?php
+$link -> close();
+?>
