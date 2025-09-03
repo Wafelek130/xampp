56fd7f8b96c81d5587fa0a1d7d3d5eb776143b4c
Walkowiak <wafelek130@gmail.com>
2025-09-03 11:18:19 +0200

    first commit

---
+++ bazyDanych/01_raporty/kwerendy.sql
@@ -0,0 +1,7 @@
+-- - Zapytanie 1: wybierające jedynie pola imie, telefon, szczepienie, opis z tabeli Zwierzeta
+
+-- - Zapytanie 2: wybierające jedynie pola id, imie, wlasciciel z tabeli Zwierzeta dla tych rekordów, dla których rodzaj to pies
+
+-- - Zapytanie 3: korzystające z relacji i wybierające jedynie pola imie z tabeli Zwierzeta oraz odpowiadające im pola nazwa z tabeli Uslugi
+
+-- - Zapytanie 4: zwracające średnią cenę wszystkich usług zapisanych w tabeli Uslugi
