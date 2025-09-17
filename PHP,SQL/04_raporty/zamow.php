6329227baa43b2a7b154b10322b8082f294a5a08
Walkowiak <wafelek130@gmail.com>
2025-09-17 12:29:32 +0200

    kupa

--- PHP,SQL/04_raporty/zamow.php
+++ PHP,SQL/04_raporty/zamow.php
@@ -1,3 +1,20 @@
+<?php
+$link = new mysqli('localhost','root','','obuwie');
+$model = $_POST['model'] ?? NULL;
+$rozmiar = $_POST['rozmiar'] ?? NULL;
+$liczba = $_POST['liczba'] ?? NULL;
+
+if($model && $liczba){
+    $sql ="SELECT nazwa, cena, kolor, kod_produktu, material, nazwa_pliku 
+            FROM buty
+            JOIN produkt ON produkt.model = buty.model
+            WHERE produkt.model = '$model'";
+
+    $result = $link -> query($sql);
+    $shoe = $result -> fetch_array();
+}
+
+?>
 <!DOCTYPE html>
 <html lang="pl">
 <head>
@@ -13,7 +30,14 @@
     <main>
         <h2>Zamówienie</h2>
         <!-- skrypt3 -->
-         <a href="http://index.php">Strona Główna</a>
+        <?php
+            echo "<img src='{$shoe['nazwa_pliku']}' alt='but męski'>
+                    <h2>{$shoe['nazwa']}</h2>
+                    <p>Cena za $liczba par: ".$liczba*$shoe['cena']." zł</p>
+                    <p>Szczegóły produktu: {$shoe['kolor']}, {$shoe['material']}</p>
+                    <p>Rozmiar: $rozmiar</p>";
+        ?>
+        <a href="index.php">Strona Główna</a>
     </main>
     <footer>
         <p>Autor strony: olekcvyl</p>
